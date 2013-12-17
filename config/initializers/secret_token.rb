7193a989abd14dd1a58e2449dd081a41fd440bf6
anjali jaiswal <anjali.swt1@gmail.com>
2013-12-17 12:09:18 +0530

    My first commit
    t remote add origin https://github.com/anjalijaiswal/depot.git#	new file:   vendor/assets/stylesheets/.keep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '3632bdbbb66da58a7ffd258cf80f4ee918f1fe097059c00e41770e1f4e352398887e79c803afe0abd2d8ecca21f40ebca8b66c4936a4758808d086de80b113c7'
